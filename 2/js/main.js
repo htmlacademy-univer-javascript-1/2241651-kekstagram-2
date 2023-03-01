42d551d9b322d443947232035f015fa0ffd95799
Keks <keks@htmlacademy.ru>
2023-03-01 08:49:19 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/main.js
@@ -0,0 +1,33 @@
+function getRandomIntInclusive(from, to){
+  if (from < 0 || to < 0) {
+    throw new RangeError ('Числа в диапазоне должны быть положительными');
+  }
+
+  if (typeof from === 'string' || typeof to === 'string') {
+    throw new RangeError ('Значения должны быть чиcлами, а не строкой');
+  }
+
+  if (from > to) {
+    [from, to] = [to, from];
+  }
+
+  if ( from === to) {
+    return to;
+  }
+
+  from = Math.ceil(from);
+  to = Math.floor(to);
+  return Math.floor(Math.random() * (to - from + 1)) + from; //Максимум и минимум включаются
+}
+
+const isCorrectLength = (str, maxLength) => {
+  if (typeof str !== 'string') {
+    throw new RangeError('Значение str должно быть строкой');//взято из предыдущей пары в Teams
+  }
+
+  return str.length <= maxLength;
+};
+
+export {getRandomIntInclusive, isCorrectLength};
+//ссылка на источник:https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
